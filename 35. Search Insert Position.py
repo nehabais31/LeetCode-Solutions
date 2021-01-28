0279e6efbb2d98688f7420c59aaeabf55f17e0e6
Neha Bais <nehabais31@gmail.com>
2021-01-27 21:07:35 -0500

    Time Complexity: O(logn)

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -17,17 +17,17 @@ class Solution:
             if target == nums[mid]:
                 return mid
             elif target > nums[mid]:
-                low = mid + 1
+                low = mid + 1   
             elif target < nums[mid]:
                 high = mid - 1
                 
         
-        return mid if target < nums[mid] else mid+1 
+        return low 
         
 
 sol = Solution()
 nums = [1,3,5,6]
-target = 7
+target = 4
 
 idx =  sol.searchInsert(nums, target)       
 print(idx)
