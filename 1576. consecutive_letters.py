edbaac26babc329ece0c82128a487b1a8c6163f6
Neha Bais <nehabais31@gmail.com>
2021-05-07 13:04:11 -0400

    Solved

---
+++ 1576. consecutive_letters.py
@@ -0,0 +1,14 @@
+# class Solution:
+#     def modifyString(self, s: str) -> str:
+        
+    
+
+# sol = Solution()
+# print(sol.modifyString("?zs"))
+# print(sol.modifyString("ubv?w"))
+
+print(ord('a')) # 97
+print(ord('z'))  # 122
+
+import random
+print(range(97, 122))
